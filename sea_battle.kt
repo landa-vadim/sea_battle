d16837f16faa3b093fa2324b7a4b5319ef73c198
landa-vadim <porvalrubahu@gmail.com>
2024-03-19 17:45:02 +0300

    random side ships

--- sea_battle.kt
+++ sea_battle.kt
@@ -2,43 +2,118 @@ fun main() {
     val myField = Array(10) { Array(10) { 0 } }
     val pcField = Array(10) { Array(10) { 0 } }
     var win = false
-
+    var myShipsCounter = 0
+    var pcShipsCounter = 0
+//    val oneDeckShipAmount = 3
+//    val twoDeckShipAmount = 2
+//    val threeDeckShipAmount = 1
+//    val fourDeckShipAmount = 1
 
 //    val predicate: (Int) -> Boolean = { it == 1 }
 
-    for (i in 1..3) {
-        createOneDeckShip(myField)
-        createOneDeckShipPc(pcField)
-        if (i == 1) {
-            createThreeDeckShip(myField)
-            createThreeDeckShipPc(pcField)
-            createFourDeckShip(myField)
-            createFourDeckShipPc(pcField)
+
+    while (myShipsCounter < 3) {
+        if (createOneDeckShip(myField)) {
+            myShipsCounter++
+        } else continue
+    }
+    while (pcShipsCounter < 3) {
+        if (createOneDeckShip(pcField)) {
+            pcShipsCounter++
+        } else continue
+    }
+    while (myShipsCounter < 5) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createTwoDeckShipColumn(myField)) {
+                myShipsCounter++
+            } else continue
+        } else {
+            if (createTwoDeckShipRow(myField)) {
+                myShipsCounter++
+            } else continue
         }
-        if (i > 1) {
-            createTwoDeckShip(myField)
-            createTwoDeckShipPc(pcField)
+    }
+    while (pcShipsCounter < 5) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createTwoDeckShipColumn(pcField)) {
+                pcShipsCounter++
+            } else continue
+        } else {
+            if (createTwoDeckShipRow(pcField)) {
+                pcShipsCounter++
+            } else continue
+        }
+    }
+    while (myShipsCounter < 6) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createThreeDeckShipColumn(myField)) {
+                myShipsCounter++
+            } else continue
+        } else {
+            if (createThreeDeckShipRow(myField)) {
+                myShipsCounter++
+            } else continue
+        }
+    }
+    while (pcShipsCounter < 6) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createThreeDeckShipColumn(pcField)) {
+                pcShipsCounter++
+            } else continue
+        } else {
+            if (createThreeDeckShipRow(pcField)) {
+                pcShipsCounter++
+            } else continue
+        }
+    }
+    while (myShipsCounter < 7) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createFourDeckShipColumn(myField)) {
+                myShipsCounter++
+            } else continue
+        } else {
+            if (createFourDeckShipRow(myField)) {
+                myShipsCounter++
+            } else continue
+        }
+    }
+    while (pcShipsCounter < 7) {
+        val shipSide = 0..1
+        if (shipSide.random() == 0) {
+            if (createFourDeckShipColumn(pcField)) {
+                pcShipsCounter++
+            } else continue
+        } else {
+            if (createFourDeckShipRow(pcField)) {
+                pcShipsCounter++
+            } else continue
         }
     }
 
-    printMyField(myField)
+printField(myField)
 
-    while (win == false) {
-
-
-
-
-    }
+//    while (!win) {
+//
+//
+//
+//
+//    }
 
 
 }
 
-fun createOneDeckShipPc(pcField: Array<Array<Int>>): Boolean {
+
+fun createOneDeckShip(field: Array<Array<Int>>): Boolean {
 
     val rowIndex = (0..9).random()
     val columnIndex = (0..9).random()
 
-    if (pcField[rowIndex][columnIndex] == 0) {
+    if (field[rowIndex][columnIndex] == 0) {
 
         val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
         val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
@@ -51,7 +126,7 @@ fun createOneDeckShipPc(pcField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (pcField[x][y] == 0 || pcField[x][y] == 2) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
                     continue
                 } else {
                     return false
@@ -62,9 +137,9 @@ fun createOneDeckShipPc(pcField: Array<Array<Int>>): Boolean {
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
                 if (x != rowIndex || y != columnIndex) {
-                    pcField[x][y] = 2
+                    field[x][y] = 2
                 } else {
-                    pcField[x][y] = 1
+                    field[x][y] = 1
                 }
             }
         }
@@ -74,12 +149,12 @@ fun createOneDeckShipPc(pcField: Array<Array<Int>>): Boolean {
     }
 }
 
-fun createTwoDeckShipPc(pcField: Array<Array<Int>>): Boolean {
+fun createTwoDeckShipColumn(field: Array<Array<Int>>): Boolean {
 
     val rowIndex = (0..9).random()
     val columnIndex = (0..8).random()
 
-    if (pcField[rowIndex][columnIndex] == 0 && pcField[rowIndex][columnIndex + 1] == 0) {
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex][columnIndex + 1] == 0) {
 
         val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
         val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
@@ -92,7 +167,7 @@ fun createTwoDeckShipPc(pcField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (pcField[x][y] == 0 || pcField[x][y] == 2) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
                     continue
                 } else {
                     return false
@@ -103,9 +178,9 @@ fun createTwoDeckShipPc(pcField: Array<Array<Int>>): Boolean {
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
                 if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1) {
-                    pcField[x][y] = 1
+                    field[x][y] = 1
                 } else {
-                    pcField[x][y] = 2
+                    field[x][y] = 2
                 }
             }
         }
@@ -115,98 +190,15 @@ fun createTwoDeckShipPc(pcField: Array<Array<Int>>): Boolean {
     }
 }
 
-fun createThreeDeckShipPc(pcField: Array<Array<Int>>): Boolean {
+fun createTwoDeckShipRow(field: Array<Array<Int>>): Boolean {
 
-    val rowIndex = (0..9).random()
-    val columnIndex = (0..7).random()
-
-    if (pcField[rowIndex][columnIndex] == 0 && pcField[rowIndex][columnIndex + 1] == 0 && pcField[rowIndex][columnIndex + 2] == 0) {
-
-        val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
-        val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
-
-        val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
-        val maxColumIndex = if (columnIndex + 2 == 9) 9 else columnIndex + 3
-
-        val rowIndexRange = minRowIndex..maxRowIndex
-        val columnIndexRange = minColumnIndex..maxColumIndex
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (pcField[x][y] == 0 || pcField[x][y] == 2) {
-                    continue
-                } else {
-                    return false
-                }
-            }
-        }
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1 || x == rowIndex && y == columnIndex + 2) {
-                    pcField[x][y] = 1
-                } else {
-                    pcField[x][y] = 2
-                }
-            }
-        }
-        return true
-    } else {
-        return false
-    }
-}
-
-fun createFourDeckShipPc(pcField: Array<Array<Int>>): Boolean {
-
-    val rowIndex = (0..9).random()
-    val columnIndex = (0..6).random()
-
-    if (pcField[rowIndex][columnIndex] == 0 && pcField[rowIndex][columnIndex + 1] == 0 && pcField[rowIndex][columnIndex + 2] == 0 && pcField[rowIndex][columnIndex + 3] == 0) {
-
-        val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
-        val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
-
-        val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
-        val maxColumIndex = if (columnIndex + 3 == 9) 9 else columnIndex + 4
-
-        val rowIndexRange = minRowIndex..maxRowIndex
-        val columnIndexRange = minColumnIndex..maxColumIndex
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (pcField[x][y] == 0 || pcField[x][y] == 2) {
-                    continue
-                } else {
-                    return false
-                }
-            }
-        }
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1 || x == rowIndex && y == columnIndex + 2 || x == rowIndex && y == columnIndex + 3) {
-                    pcField[x][y] = 1
-                } else {
-                    pcField[x][y] = 2
-                }
-            }
-        }
-        return true
-    } else {
-        return false
-    }
-}
-
-
-fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
-
-    val rowIndex = (0..9).random()
+    val rowIndex = (0..8).random()
     val columnIndex = (0..9).random()
 
-    if (myField[rowIndex][columnIndex] == 0) {
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex + 1][columnIndex] == 0) {
 
         val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
-        val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
+        val maxRowIndex = if (rowIndex + 1 == 9) 9 else rowIndex + 2
 
         val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
         val maxColumIndex = if (columnIndex + 1 > 9) 9 else columnIndex + 1
@@ -216,7 +208,7 @@ fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (myField[x][y] == 0 || myField[x][y] == 2) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
                     continue
                 } else {
                     return false
@@ -226,10 +218,10 @@ fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (x != rowIndex || y != columnIndex) {
-                    myField[x][y] = 2
+                if (x == rowIndex && y == columnIndex || x == rowIndex + 1 && y == columnIndex) {
+                    field[x][y] = 1
                 } else {
-                    myField[x][y] = 1
+                    field[x][y] = 2
                 }
             }
         }
@@ -239,54 +231,12 @@ fun createOneDeckShip(myField: Array<Array<Int>>): Boolean {
     }
 }
 
-
-fun createTwoDeckShip(myField: Array<Array<Int>>): Boolean {
-
-    val rowIndex = (0..9).random()
-    val columnIndex = (0..8).random()
-
-    if (myField[rowIndex][columnIndex] == 0 && myField[rowIndex][columnIndex + 1] == 0) {
-
-        val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
-        val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
-
-        val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
-        val maxColumIndex = if (columnIndex + 1 == 9) 9 else columnIndex + 2
-
-        val rowIndexRange = minRowIndex..maxRowIndex
-        val columnIndexRange = minColumnIndex..maxColumIndex
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (myField[x][y] == 0 || myField[x][y] == 2) {
-                    continue
-                } else {
-                    return false
-                }
-            }
-        }
-
-        for (x in rowIndexRange) {
-            for (y in columnIndexRange) {
-                if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1) {
-                    myField[x][y] = 1
-                } else {
-                    myField[x][y] = 2
-                }
-            }
-        }
-        return true
-    } else {
-        return false
-    }
-}
-
-fun createThreeDeckShip(myField: Array<Array<Int>>): Boolean {
+fun createThreeDeckShipColumn(field: Array<Array<Int>>): Boolean {
 
     val rowIndex = (0..9).random()
     val columnIndex = (0..7).random()
 
-    if (myField[rowIndex][columnIndex] == 0 && myField[rowIndex][columnIndex + 1] == 0 && myField[rowIndex][columnIndex + 2] == 0) {
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex][columnIndex + 1] == 0 && field[rowIndex][columnIndex + 2] == 0) {
 
         val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
         val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
@@ -299,7 +249,7 @@ fun createThreeDeckShip(myField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (myField[x][y] == 0 || myField[x][y] == 2) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
                     continue
                 } else {
                     return false
@@ -310,9 +260,9 @@ fun createThreeDeckShip(myField: Array<Array<Int>>): Boolean {
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
                 if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1 || x == rowIndex && y == columnIndex + 2) {
-                    myField[x][y] = 1
+                    field[x][y] = 1
                 } else {
-                    myField[x][y] = 2
+                    field[x][y] = 2
                 }
             }
         }
@@ -322,12 +272,54 @@ fun createThreeDeckShip(myField: Array<Array<Int>>): Boolean {
     }
 }
 
-fun createFourDeckShip(myField: Array<Array<Int>>): Boolean {
+fun createThreeDeckShipRow(field: Array<Array<Int>>): Boolean {
+
+    val rowIndex = (0..7).random()
+    val columnIndex = (0..9).random()
+
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex + 1][columnIndex] == 0 && field[rowIndex + 2][columnIndex] == 0) {
+
+        val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
+        val maxRowIndex = if (rowIndex + 2 == 9) 9 else rowIndex + 3
+
+        val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
+        val maxColumIndex = if (columnIndex + 1 > 9) 9 else columnIndex + 1
+
+        val rowIndexRange = minRowIndex..maxRowIndex
+        val columnIndexRange = minColumnIndex..maxColumIndex
+
+        for (x in rowIndexRange) {
+            for (y in columnIndexRange) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+
+        for (x in rowIndexRange) {
+            for (y in columnIndexRange) {
+                if (x == rowIndex && y == columnIndex || x == rowIndex + 1 && y == columnIndex || x == rowIndex + 2 && y == columnIndex) {
+                    field[x][y] = 1
+                } else {
+                    field[x][y] = 2
+                }
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+
+fun createFourDeckShipColumn(field: Array<Array<Int>>): Boolean {
 
     val rowIndex = (0..9).random()
     val columnIndex = (0..6).random()
 
-    if (myField[rowIndex][columnIndex] == 0 && myField[rowIndex][columnIndex + 1] == 0 && myField[rowIndex][columnIndex + 2] == 0 && myField[rowIndex][columnIndex + 3] == 0) {
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex][columnIndex + 1] == 0 && field[rowIndex][columnIndex + 2] == 0 && field[rowIndex][columnIndex + 3] == 0) {
 
         val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
         val maxRowIndex = if (rowIndex + 1 > 9) 9 else rowIndex + 1
@@ -340,7 +332,7 @@ fun createFourDeckShip(myField: Array<Array<Int>>): Boolean {
 
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
-                if (myField[x][y] == 0 || myField[x][y] == 2) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
                     continue
                 } else {
                     return false
@@ -351,9 +343,9 @@ fun createFourDeckShip(myField: Array<Array<Int>>): Boolean {
         for (x in rowIndexRange) {
             for (y in columnIndexRange) {
                 if (x == rowIndex && y == columnIndex || x == rowIndex && y == columnIndex + 1 || x == rowIndex && y == columnIndex + 2 || x == rowIndex && y == columnIndex + 3) {
-                    myField[x][y] = 1
+                    field[x][y] = 1
                 } else {
-                    myField[x][y] = 2
+                    field[x][y] = 2
                 }
             }
         }
@@ -363,7 +355,49 @@ fun createFourDeckShip(myField: Array<Array<Int>>): Boolean {
     }
 }
 
-fun printMyField(myField: Array<Array<Int>>) {
+fun createFourDeckShipRow(field: Array<Array<Int>>): Boolean {
+
+    val rowIndex = (0..6).random()
+    val columnIndex = (0..9).random()
+
+    if (field[rowIndex][columnIndex] == 0 && field[rowIndex + 1][columnIndex] == 0 && field[rowIndex + 2][columnIndex] == 0 && field[rowIndex + 3][columnIndex] == 0) {
+
+        val minRowIndex = if (rowIndex - 1 < 0) 0 else rowIndex - 1
+        val maxRowIndex = if (rowIndex + 3 == 9) 9 else rowIndex + 4
+
+        val minColumnIndex = if (columnIndex - 1 < 0) 0 else columnIndex - 1
+        val maxColumIndex = if (columnIndex + 1 > 9) 9 else columnIndex + 1
+
+        val rowIndexRange = minRowIndex..maxRowIndex
+        val columnIndexRange = minColumnIndex..maxColumIndex
+
+        for (x in rowIndexRange) {
+            for (y in columnIndexRange) {
+                if (field[x][y] == 0 || field[x][y] == 2) {
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+
+        for (x in rowIndexRange) {
+            for (y in columnIndexRange) {
+                if (x == rowIndex && y == columnIndex || x == rowIndex + 1 && y == columnIndex || x == rowIndex + 2 && y == columnIndex || x == rowIndex + 3 && y == columnIndex) {
+                    field[x][y] = 1
+                } else {
+                    field[x][y] = 2
+                }
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+
+fun printField(field: Array<Array<Int>>) {
 
     val letterArray = charArrayOf('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К')
     var number0 = 0
@@ -376,7 +410,7 @@ fun printMyField(myField: Array<Array<Int>>) {
         print(" ")
     }
     println()
-    for (row in myField) {
+    for (row in field) {
 
         if (number1 < 10) {
             print("${number1++} |")
@@ -393,32 +427,3 @@ fun printMyField(myField: Array<Array<Int>>) {
     }
 }
 
-fun printPcField(pcField: Array<Array<Int>>) {
-
-    val letterArray = charArrayOf('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К')
-    var number0 = 0
-    var number1 = 1
-
-    println()
-    print("    ")
-    for (i in letterArray) {
-        print(letterArray[number0++])
-        print(" ")
-    }
-    println()
-    for (row in pcField) {
-
-        if (number1 < 10) {
-            print("${number1++} |")
-        } else {
-            print("$number1|")
-        }
-        print(" ")
-
-        for (column in row) {
-
-            print("$column ")
-        }
-        println()
-    }
-}
